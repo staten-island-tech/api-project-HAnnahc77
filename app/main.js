23dce4fbc5b26a895844eba8935e1c3b57ff7287
HAnnahc77 <HAnnahc77@nycstudents.net>
2024-11-18 14:01:15 -0500

    commir

--- app/main.js
+++ app/main.js
@@ -1,1 +1,17 @@
 import "./style.css";
+
+async function getData() {
+  try {
+    const response = await fetch("https://api.disneyapi.dev/character");
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data.data);
+    }
+  } catch (error) {
+    alert("hey i cant find that");
+  }
+}
+
+getData();
