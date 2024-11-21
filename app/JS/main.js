4b8fff2bdb17697f596815edc444abae206fad75
HAnnahc77 <HAnnahc77@nycstudents.net>
2024-11-21 14:00:17 -0500

    commit

--- app/JS/main.js
+++ app/JS/main.js
@@ -5,9 +5,15 @@ const URL = "https://api.disneyapi.dev/character";
 async function getData(URL) {
   try {
     const response = await fetch(URL);
-    const data = await response.json();
-    console.log(data.data);
-  } catch (error) {}
+    if (response.status !== 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+    }
+  } catch (error) {
+    alert("Unable to find all information.");
+  }
 }
 
 getData(URL);
