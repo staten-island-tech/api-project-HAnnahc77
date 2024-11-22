5f8c5c6f91b8366c402ee5aa3fa3d9a6c7e2fd98
HAnnahc77 <HAnnahc77@nycstudents.net>
2024-11-22 10:59:30 -0500

    commit

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,8 +1,10 @@
 import "../CSS/style.css";
+import "../CSS/tailwind.css";
+import { DOMSelectors } from "./selectors.js";
 
 const URL = "https://api.disneyapi.dev/character";
 
-async function getData(URL){
+async function getData(URL) {
   try {
     const response = await fetch(URL);
     if (response.status !== 200) {
@@ -10,10 +12,25 @@ async function getData(URL){
     } else {
       const data = await response.json();
       console.log(data);
+      data.data.forEach((character) => {
+        insertCards(character);
+      });
     }
   } catch (error) {
     alert("Unable to find all information.");
   }
+}
+
+function insertCards(character) {
+  DOMSelectors.container.insertAdjacentHTML(
+    "beforeend",
+    `
+      <div class="character-card">
+        <h2>${character.name}</h2>
+        <img src="${character.imageUrl}" alt="Image of ${character.name}" />
+      </div>
+    `
+  );
 }
 
 getData(URL);
